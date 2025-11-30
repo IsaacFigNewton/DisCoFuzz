9fbb5a85d1fea82ca400c933fddc761fd1def228
Isaac Rudnick <48895941+IsaacFigNewton@users.noreply.github.com>
2025-11-29 18:40:42 -0800

    updated spacy dependency parser class
    also updated WiC eval notebook to speed up embedding, fuzzification processes

--- src/discofuzz/SpacyDependencyComposer.py
+++ src/discofuzz/SpacyDependencyComposer.py
@@ -1,3 +1,4 @@
+import numpy as np
 import spacy
 import tensorflow as tf
 from .constants import STRATEGIES
@@ -5,35 +6,25 @@ from .LemmaVectorizer import LemmaVectorizer
 from .fuzzy_classes.FuzzyFourierTensorTransformer import FuzzyFourierTensorTransformer
 
 class SpacyDependencyComposer:
+  modifiers = {"v", "a"}
+
   def __init__(self,
       strategy:str=None,
-      spacy_model=None,
-      lemma_vectorizer=None,
       fuzzifier=None
     ):
     if strategy not in STRATEGIES:
       raise ValueError(f"Unknown strategy: {strategy}")
     self.strategy = strategy
-    self.nlp = spacy.load("en_core_web_sm") if spacy_model is None else spacy_model
-    self.lemma_vectorizer = LemmaVectorizer() if lemma_vectorizer is None else lemma_vectorizer
     self.fuzzifier = FuzzyFourierTensorTransformer() if fuzzifier is None else fuzzifier
 
-  def _compose_tok_embedding(self, token) -> tf.Tensor:
-    # lemma_vectorizer returns a numpy array, so convert it to tf.Tensor for fuzzification
-    current_tok_tens = self.lemma_vectorizer(token.lemma_.lower())
-    current_tok_tens = tf.convert_to_tensor(current_tok_tens, dtype=tf.float32)
-    current_tok_tens = self.fuzzifier.fuzzify(current_tok_tens)
+  def _compose_tok_embedding(self, branch: tuple | np.ndarray, root_pos: str = 'n') -> tf.Tensor:
 
-    if token.children:
+    if isinstance(branch, tuple):
       # get all the childrens'embeddings
       child_embeddings = [
-          self._compose_tok_embedding(c)
-          for c in token.children
+          self._compose_tok_embedding(e)
+          for e in branch
       ]
-
-      if not child_embeddings:
-        # If no valid children, return the current token's embedding
-        return current_tok_tens
 
       # compose child embeddings based on strategy
       match self.strategy:
@@ -64,7 +55,7 @@ class SpacyDependencyComposer:
 
         case "selective_intersection+mean":
           # if the current token is a modifier of some kind, intersect it with all its children
-          if token.pos_ in {"VERB", "ADJ", "ADV"}:
+          if root_pos in self.modifiers:
             child_embeddings_intersected = [
                 self.fuzzifier.intersection(current_tok_tens, c)
                 for c in child_embeddings
@@ -75,7 +66,7 @@ class SpacyDependencyComposer:
 
         case "selective_intersection+union":
           # if the current token is a modifier of some kind, intersect it with all its children
-          if token.pos_ in {"VERB", "ADJ", "ADV"}:
+          if root_pos in self.modifiers:
             child_embeddings_intersected = [
                 self.fuzzifier.intersection(current_tok_tens, c)
                 for c in child_embeddings
@@ -87,7 +78,7 @@ class SpacyDependencyComposer:
 
         case "selective_intersection+intersection+mean":
           # if the current token is a modifier of some kind, intersect it with all its children
-          if token.pos_ in {"VERB", "ADJ", "ADV"}:
+          if root_pos in self.modifiers:
             child_embeddings_intersected = [
                 self.fuzzifier.intersection(current_tok_tens, c)
                 for c in child_embeddings
@@ -105,21 +96,12 @@ class SpacyDependencyComposer:
       return current_tok_tens
 
   def __call__(self,
-      text: str,
+      branch: tuple,
+      root_pos: str
   ) -> tf.Tensor:
       # baseline
       if self.strategy is None:
-          return self.fuzzifier.fuzzify(self.lemma_vectorizer(text))
+          root_emb = [e for e in branch if type(e) == np.ndarray]
+          return root_emb[0]
 
-      doc = self.nlp(text)
-      root_embeddings = []
-      for sent in doc.sents:
-          root_emb = self._compose_tok_embedding(sent.root)
-          if root_emb is not None:
-            root_embeddings.append(root_emb)
-
-      if not root_embeddings:
-          return None
-
-      # get the average
-      return tf.reduce_mean(tf.stack(root_embeddings), axis=0)
+      return self._compose_tok_embedding(branch, root_pos)
