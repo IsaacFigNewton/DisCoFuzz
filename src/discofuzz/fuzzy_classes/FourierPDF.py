fixed intersection, part of normalization

src/discofuzz/fuzzy_classes/FourierPDF.py
@@ -120,7 +120,8 @@ class FourierPDF:
 
         bounds = tf.constant([lb, ub])
         cdf = self._get_cdf_batch(a)
-        cdf_lb_ub = self.evaluate_batch(cdf, x=bounds)
+        cdf_mag = self.get_npsd_batch(cdf)
+        cdf_lb_ub = self.evaluate_batch(cdf_mag, x=bounds)
         # integral is just cdf evaluated at 1 - cdf evaluated at 0
         return cdf_lb_ub[:, 1] - cdf_lb_ub[:, 0]
 

src/discofuzz/fuzzy_classes/FuzzyFourierSetMixin.py
@@ -95,6 +95,6 @@ class FuzzyFourierSetMixin(FourierPDF):
         """
         if len(tf.shape(a)) != 2:
             raise ValueError(f"Input tensor must have shape (batch_size, kernel_size), received tensor of shape {tf.shape(a)}")
-        result = a + b #- self._convolve_batch(a, b)
+        result = a + b - self.intersection_batch(a, b)
         result = self._normalize_batch(result)
         return result